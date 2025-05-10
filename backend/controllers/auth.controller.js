481a6c33839c1e443f5c5999b93ff0ef6d4fcc38
ShreeVarshana <shreevarshanar.22cse@kongu.edu>
2025-05-10 17:41:14 +0530

    signup defined

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,72 @@
-export const signup = (req, res) => {
-    res.send("SINGUP");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+    try {
+        const { username, fullName, email, password } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid emial format" });
+        }
+
+        const existingEmail = await User.findOne({ email: email })
+        const existingUsername = await User.findOne({ username })
+
+        if (existingEmail || existingUsername) {
+            return res.status(400).json({ error: "Already the username or the email exists" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ error: "Password must have atleast 6 character length" });
+        }
+
+        //hashing the password
+        //123456 = ced43romieoe4ri22
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            fullName,
+            email,
+            password: hashedPassword
+
+        })
+
+        if (newUser) {
+            await newUser.save();
+            res.status(200).json({
+                //        message: "User Created Successfully"
+
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following
+            });
+
+        }
+        else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
+
+    } catch (err) {
+        console.log(`Error in signup controller ${err}`);
+        res.status(500).json({ error: "Internal server Error" });
+    }
+
+
+}
+
+
+
+
+export const login = (req, res) => {
+    res.send("login controller");
+}
+export const logout = (req, res) => {
+    res.send("logout controller");
 }
--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,16 @@ import dotenv from "dotenv";
 import express from "express";
 
 import authRoute from "./routes/auth.route.js";
+import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 const app = express(); // server instance using this we can make the routes, middlewares etc...
 const PORT = process.env.PORT;
 
-
+app.use(express.json())
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
     console.log("Server is running on the port 5000")
+    connectDB();
 })
