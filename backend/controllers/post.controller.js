052fc7ba5e6c186e89cab699a092182fa9dce319
ShreeVarshana <shreevarshanar.22cse@kongu.edu>
2025-05-24 22:42:59 +0530

    post delete controller defined

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -35,3 +35,38 @@ export const createPost = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 }
+
+
+//----------------------------------------------------------------------------------//
+
+export const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+
+        if (post.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ error: "You are not authorized to delete the post" });
+        }
+
+
+        if (post.img) {
+            const imgId = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
+
+
+        await Post.findByIdAndDelete(id);
+
+        res.status(200).json({ message: "Post deleted successfully" });
+
+    } catch (err) {
+        console.error(`Error in deletion of the post: ${err}`);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
