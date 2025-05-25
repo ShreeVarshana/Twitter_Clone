9ae3b4882ead9ef89dde2ef950ebeafb6e89e81a
ShreeVarshana <shreevarshanar.22cse@kongu.edu>
2025-05-25 21:35:41 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import connectDB from "./db/connectDB.js";
 import postRoute from "./routes/post.route.js";
+import NotificationRoute from "./routes/notification.route.js";
 
 
 dotenv.config();
@@ -29,8 +30,8 @@ const PORT = process.env.PORT;
 app.use(express.json())
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
 app.use("/api/posts", postRoute);
+app.use("/api/Notifications", NotificationRoute);
 
 
 app.listen(PORT, () => {
